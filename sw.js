6b6ab496d263c97659230cf2f5aa9347fab13b5c
skodama1 <128713968+skodama1@users.noreply.github.com>
2026-06-13 22:32:09 +0900

    update61352 + remove premiere working files

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: アセットキャッシュ（オフライン対応 & リピート高速化）
-const CACHE_NAME = 'novel-game-obf-dpets3';
+const CACHE_NAME = 'novel-game-obf-1nvexr';
 
 // キャッシュ対象の拡張子
 const CACHEABLE_EXT = /\.(mp4|webm|ogg|mp3|wav|m4a|aac|png|jpg|jpeg|gif|webp|svg|woff2?)$/i;
